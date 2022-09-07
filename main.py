ba13880fe74e9efe10be044ce42f8767e9a1e0aa
eval Nya <60149113+nexplorer-3e@users.noreply.github.com>
2022-09-07 17:27:14 +0800

    TODO: getGiteeData

--- main.py
+++ main.py
@@ -8,6 +8,32 @@ from config import *
 
 def getGiteeData(plist):
     # todo: show specific branch status not only newest commit
+    # get git source data, copied from src-oe-verinfo
+    for pkg in plist:
+        # print ('obs package', pkg)
+        for s in projRepoUrl:
+            service_url = projRepoUrl[s].replace("project/monitor", "package/view_file") + '{}/_service'.format(pkg)
+            service_resp = requests.get(service_url, auth=HTTPBasicAuth(account['user'], account['password']))
+            service_data = service_resp.text
+        # print ('service_data', service_data)
+        git_pattern = '<param name="url">.*</param>'
+        revision_pattern = '<param name="revision">.*</param>'
+        try:
+            gitinfo = re.search(git_pattern, service_data).group()
+            revision = re.search(revision_pattern, service_data).group()
+            # print (gitinfo)
+            # print (revision)
+            gitinfo = re.search('>.*<', gitinfo).group()[1:-1]
+            revision = re.search('>.*<', revision).group()[1:-1]
+            if len(revision) == 0:
+                revision = 'None'
+            # print ('git', gitinfo)
+            # print ('revision', revision)
+        except:
+            gitinfo = 'None'
+            revision = 'None'
+            print('Cannot get url and revision!')
+
     for p in plist:
 
     pass
